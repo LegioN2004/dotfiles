d5fc3222c7d0a7829849a2e6909867481a2081f2
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2022-09-18 14:07:16 +0530

    added hop nvim plugin

--- nvim/.config/nvim/lua/legion/plugins.lua
+++ nvim/.config/nvim/lua/legion/plugins.lua
@@ -71,6 +71,15 @@ return require 'packer'.startup(function()
 		'svrana/neosolarized.nvim',
 		requires = { 'tjdevries/colorbuddy.nvim' }
 	}
+	--hop.nvim
+	use {
+    'phaazon/hop.nvim',
+  branch = 'v2', -- optional but strongly recommended
+  config = function()
+    -- you can configure Hop the way you like here; see :h hop-config
+    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+  end
+   }
 	--language specific stuff
 	use 'tpope/vim-commentary'
 	use 'windwp/nvim-ts-autotag'
