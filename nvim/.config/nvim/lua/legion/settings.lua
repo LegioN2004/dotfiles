c6fb478e472e4f49409404f3ebea8eed2d695423
LegioN2004 <87975145+LegioN2004@users.noreply.github.com>
2022-09-21 16:28:14 +0530

    added neovide settings

--- nvim/.config/nvim/lua/legion/settings.lua
+++ nvim/.config/nvim/lua/legion/settings.lua
@@ -92,5 +92,15 @@ endfunction
 vim.cmd([[
 "font for neovide
 set guifont=Hack\ NF:h12
-"check
+
+"neovide stuff
+if exists("g:neovide")   " Put anything you want to happen only in Neovide here
+	let g:neovide_refresh_rate=60
+	let g:neovide_refresh_rate_idle=5
+	let g:neovide_transparency=0.8
+	let g:neovide_refresh_rate=60
+	let g:neovide_refresh_rate_idle=5
+	let g:neovide_cursor_vfx_mode = "railgun"
+	let g:neovide_remember_window_size = v:true
+endif
 ]])
