11b82822df9fa5797f64c6af90f5a25bd20bb93c
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2022-10-02 10:28:23 +0530

    added command to run rust files

--- nvim/.config/nvim/lua/legion/macros.lua
+++ nvim/.config/nvim/lua/legion/macros.lua
@@ -10,6 +10,7 @@ vim.cmd [[ let @t=":ProjectRoot \<CR>:! go test ./... -coverprofile=coverage.out
 vim.cmd [[
 	augroup run_file
 		autocmd BufEnter *.java let @g=":w\<CR>:vsp | terminal java %\<CR>i"
+		autocmd BufEnter *.rs let @g=":w\<CR>:vsp | terminal rustc %\<CR>i"
 		autocmd BufEnter *.py let @g=":w\<CR>:vsp |terminal python %\<CR>i"
 		autocmd BufEnter *.asm let @g=":w\<CR> :!nasm -f elf64 -o out.o % && ld out.o -o a.out \<CR> | :vsp |terminal ./a.out\<CR>i"
 		autocmd BufEnter *.cpp let @g=":w\<CR> :!g++ -std=c++17 -O3 %\<CR> | :vsp |terminal ./a.out\<CR>i"
