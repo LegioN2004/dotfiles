d4d372c032936069be056c5695c9d7eed645be3d
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2022-10-12 23:25:36 +0530

    removed

--- windows neovim/nvim/lua/legion/neovide.lua
+++
@@ -1,8 +0,0 @@
-if vim.g.neovide == true then
-	-- vim.cmd 'set guifont=Hack\ NF:h10'
-	-- vim.o.guifont='Consolas:h10'
-	-- vim.o.guifont='FiraCode NF:h14'
-	vim.cmd([[ colorscheme neovide ]])
-	vim.o.guifont = 'Hack NF:h11'
-	vim.api.nvim_set_keymap('n', '<F11>', ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>", {})
-end
