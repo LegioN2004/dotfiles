9e792f6ba0a363cefc5643520d45a75a318ec68b
LegioN2004 <sunnybaruasins@gmail.com>
2022-10-21 17:40:08 +0530

    nothing again

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -180,6 +180,11 @@ _G.packer_plugins = {
     path = "/home/sunny/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-lsp"] = {
+    loaded = true,
+    path = "/home/sunny/.local/share/nvim/site/pack/packer/start/nvim-lsp",
+    url = "https://github.com/neovim/nvim-lsp"
+  },
   ["nvim-lsp-installer"] = {
     loaded = true,
     path = "/home/sunny/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
