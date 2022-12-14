f120db5968aa8140787f65d40fbf1603bde9c00c
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2022-12-14 16:32:41 +0530

    updated telescope.lua

--- nvim/.config/nvim/lua/legion/telescope.lua
+++ nvim/.config/nvim/lua/legion/telescope.lua
@@ -7,7 +7,7 @@ local function telescope_buffer_dir()
   return vim.fn.expand('%:p:h')
 end
 
-local fb_actions = require "telescope".extensions.file_browser.actions
+-- local fb_actions = require "telescope".extensions.file_browser.actions
 
 telescope.setup {
   defaults = {
@@ -16,29 +16,30 @@ telescope.setup {
         ["q"] = actions.close
       }
     }
-  },
-  extensions = {
-    file_browser = {
-      theme = "dropdown",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        -- your custom insert mode mappings
-        ["i"] = {
-          ["<C-w>"] = function() vim.cmd('normal vbd') end,
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-          ["N"] = fb_actions.create,
-          ["h"] = fb_actions.goto_parent_dir,
-          ["/"] = function()
-            vim.cmd('startinsert')
-          end
-        }
-      }
-    }
   }
 }
+--   extensions = {
+--     file_browser = {
+--       theme = "dropdown",
+--       -- disables netrw and use telescope-file-browser in its place
+--       hijack_netrw = true,
+--       mappings = {
+--         -- your custom insert mode mappings
+--         ["i"] = {
+--           ["<C-w>"] = function() vim.cmd('normal vbd') end,
+--         },
+--         ["n"] = {
+--           -- your custom normal mode mappings
+--           ["N"] = fb_actions.create,
+--           ["h"] = fb_actions.goto_parent_dir,
+--           ["/"] = function()
+--             vim.cmd('startinsert')
+--           end
+--         }
+--       }
+--     }
+--   }
+-- }
 
 -- telescope.load_extension("file_browser")
 
