71ab996a6c334de4334c812e44ef1cd8c7e65f3f
LegioN2004 <sunnybaruasins@gmail.com>
2023-03-02 18:20:14 +0530

    anetane

--- nvim/.config/nvim/lua/legion/plugins.lua
+++ nvim/.config/nvim/lua/legion/plugins.lua
@@ -1,79 +1,106 @@
 local fn = vim.fn
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-		vim.fn.system({
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+		PACKER_BOOTSTRAP = fn.system({
 				"git",
 				"clone",
-				"--filter=blob:none",
-				"https://github.com/folke/lazy.nvim.git",
-				"--branch=stable", -- latest stable release
-				lazypath,
+				"--depth",
+				"1",
+				"https://github.com/wbthomason/packer.nvim",
+				install_path,
 		})
+		print("Installing packer close and reopen Neovim...")
+		vim.cmd([[packadd packer.nvim]])
 end
-vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerSync
+augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+		print("Packer is not installed")
+		return
+end
+
+-- Have packer use a popup window
+packer.init({
+		display = {
+				open_fn = function()
+						return require("packer.util").float({ border = "rounded" })
+				end,
+		},
+})
 
 --put plugins here
-require('lazy').setup({
-		'lewis6991/impatient.nvim',
-		{
-				"olimorris/onedarkpro.nvim",
-				priority = 1000 -- Ensure it loads first
-		},
-		'nvim-lualine/lualine.nvim',
-		{
-				-- amongst your other plugins
-				{'akinsho/toggleterm.nvim', version = "*", config = true}
-		},
-		'ThePrimeagen/vim-be-good',
-		'mbbill/undotree',
-		'lewis6991/gitsigns.nvim',
-		'szw/vim-maximizer',
-		-- 'xolox/vim-session',
-		-- 'xolox/vim-misc',
+return require 'packer'.startup(function()
+		use 'wbthomason/packer.nvim' --main packer neovim plugin manager
+		use 'lewis6991/impatient.nvim'
+		use 'nvim-lualine/lualine.nvim'
+		use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+				require("toggleterm").setup()
+		end}
+		use 'tjdevries/express_line.nvim'
+		use 'j-hui/fidget.nvim'
+		use 'ThePrimeagen/vim-be-good'
+		use 'mbbill/undotree'
+		use 'lewis6991/gitsigns.nvim'
+		use 'szw/vim-maximizer'
+		-- use 'xolox/vim-session'
+		-- use 'xolox/vim-misc'
 		-- bufferline
-		'akinsho/bufferline.nvim',
+		use 'akinsho/bufferline.nvim'
 		--colorscheme
-		{
+		use {
 				'svrana/neosolarized.nvim',
-				dependencies = { 'tjdevries/colorbuddy.nvim' }
-		},
+				requires = { 'tjdevries/colorbuddy.nvim' }
+		}
 		--language specific stuff
-		'tpope/vim-commentary',
-		'tpope/vim-surround',
-		'tpope/vim-fugitive',
+		use 'tpope/vim-commentary'
+		use 'tpope/vim-surround'
+		use 'tpope/vim-fugitive'
 
-		-- 'windwp/nvim-ts-autotag',
-		'jiangmiao/auto-pairs',
-		'nvim-tree/nvim-web-devicons',
-		{
+		-- use 'windwp/nvim-ts-autotag'
+		use 'jiangmiao/auto-pairs'
+		use 'kyazdani42/nvim-web-devicons'
+		use {
 				'nvim-treesitter/nvim-treesitter',
-				build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-		},
-		'nvim-lua/popup.nvim',
-		'nvim-lua/plenary.nvim',
-		'nvim-telescope/telescope.nvim',
+				run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+		}
+		use 'nvim-lua/popup.nvim'
+		use 'nvim-lua/plenary.nvim'
+		use 'nvim-telescope/telescope.nvim'
 		-- not using the telescope file explorer doesn't show files like ide's i need to see them without entering the folder everytime
 		-- instead this
-		{
-				'nvim-tree/nvim-tree.lua',
-				dependencies = {
-						'nvim-tree/nvim-web-devicons', -- optional, for file icons
-				},
-				version = "nightly" -- optional, updated every week. (see issue #1193)
-		},
-		'junegunn/fzf',
-		'junegunn/fzf.vim',
-		-- use = { 'junegunn/fzf', build = './install --bin', }
-		{ 'neoclide/coc.nvim', branch = "release" },
-		{
+		use {
+		  'nvim-tree/nvim-tree.lua',
+		  requires = {
+			 'nvim-tree/nvim-web-devicons', -- optional, for file icons
+		  },
+		  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+		}
+		use 'junegunn/fzf.vim'
+		-- use = { 'junegunn/fzf', run = './install --bin', }
+		use { 'neoclide/coc.nvim', branch = "release" }
+		use {
 				'goolord/alpha-nvim',
-				dependencies = { 'nvim-tree/nvim-web-devicons' },
+				requires = { 'kyazdani42/nvim-web-devicons' },
 				config = function()
 						require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
 				end
-		},
-})
+		}
+
+
+		-- Automatically set up your configuration after cloning packer.nvim
+		-- Put this at the end after all plugins
+		if PACKER_BOOTSTRAP then
+				require("packer").sync()
+		end
+end)
