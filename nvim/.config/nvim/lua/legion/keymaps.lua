a7954966ad6d96d9afd1e2faad44cb6039ed24d9
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-03-04 20:05:18 +0530

    aroitn

--- nvim/.config/nvim/lua/legion/keymaps.lua
+++ nvim/.config/nvim/lua/legion/keymaps.lua
@@ -18,12 +18,8 @@ keymap("", "<Space>", "<Nop>", opts)
 --   term_mode = "t",
 --   command_mode = "c",
 
--- Insert --
--- Press qq fast to exit insert mode
-keymap("i", "qq", "<ESC>", opts)
-
 --dont yank  with x
-vim.keymap.set('n', 'x', '"_x')
+--vim.keymap.set('n', 'x', '"_x')
 
 --increment / decrement
 vim.keymap.set('n', '+', '<C-a>')
@@ -36,7 +32,6 @@ vim.keymap.set('n', '-', '<C-x>')
 -- select all text at once
 vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
 
-
 -- -- keybinds for easier saving and quitting
 keymap("n", "tw", ":w!<CR>", opts)
 keymap("n", "tq", ":q!<CR>", opts)
@@ -44,27 +39,17 @@ keymap("n", "tqa", ":qa!<CR>", opts)
 keymap("n", "twq", ":wq!<CR>", opts)
 keymap("n", "ts", ":so ~/.config/nvim/init.lua", opts)
 
+--splitting windows
+vim.keymap.set('n', 'sv', '<c-w>v', { silent = true })
+vim.keymap.set('n', 'sh', '<c-w>s', { silent = true })
+vim.keymap.set('n', 'sx', ':close<cr>', { silent = true })
+
 --(window keybinds) new tab
 keymap("n", "tn", ":tabnew<Return><C-w>w", opts)
--- keymap("n", "te", ":tabedit<cr>", opts), { silent = true })
-
---splitting windows
-vim.keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
-vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
-vim.keymap.set('n', 'sh', '<C-w>s', { silent = true })
-vim.keymap.set('n', 'sx', ':close<CR>', { silent = true })
 
 --tab movement
 keymap("n", "<tab>", ":tabnext<Return>", opts)
 keymap("n", "<S-tab>", ":tabprevious<Return>", opts)
-
---move window
--- vim.keymap.set('n', '<Space>', '<C-w>w')
-vim.keymap.set('', 's<left>', '<C-w>h')
-vim.keymap.set('', 's<up>', '<C-w>k')
-vim.keymap.set('', 's<down>', '<C-w>j')
-vim.keymap.set('', 's<right>', '<C-w>l')
-vim.keymap.set('', 's<right>', '<C-w>l')
 
 --better movement between splits
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -78,6 +63,40 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- from ThePrimeagen ------------------------------------------
+
+-- best remap from the ThePrimeagen
+-- greatest remap ever
+vim.keymap.set('x', "<leader>p", "\"_dP")
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set('n', "<leader>y", "\"+y")
+vim.keymap.set('v', "<leader>y", "\"+y")
+vim.keymap.set('n', "<leader>Y", "\"+Y")
+
+vim.keymap.set('n', "<leader>d", "\"_d")
+vim.keymap.set('v', "<leader>d", "\"_d")
+
+-- half page up/down with centered view
+vim.keymap.set('n', "<C-u>", "<C-u>zz")
+vim.keymap.set('n', "<C-d>", "<C-d>zz")
+
+-- searching forward or back with half page up/down
+vim.keymap.set('n', "n", "nzzzv")
+vim.keymap.set('n', "N", "nzzzv")
+
+------------------------------------------------------------------
+
+-- shortcuts for commenting
+vim.keymap.set('n', "<c-/>", vim.cmd.commentary)
+vim.keymap.set('v', "<c-/>", vim.cmd.commentary)
+
+-- undotree keymaps
+vim.keymap.set('n', "<leader>un", ":undotreetoggle<cr>")
+
+-- maximizer keymap
+vim.keymap.set('n', "<leader>mt", ":maximizertoggle<cr>")
+
 vim.cmd([[
 "fzf keybindings
 nnoremap <silent> <leader>fzf :FZF ~<cr>
@@ -85,6 +104,8 @@ nnoremap <silent> <leader>fr :History<CR>
 nnoremap <silent> <leader>ff :e %:h/<C-d>
 nnoremap <silent> <leader>fi :FZF /home/sunny/ghq/github.com/legion2004/programs/ <cr>
 nnoremap <silent> <leader>dot :FZF /home/sunny/dotfiles/ <cr>
+"open files from the home folder
+"nnoremap <silent> <leader>hot :FZF~ <cr>
 
 "toggle fugitive
 nnoremap <leader>gs :Git<CR>
@@ -99,49 +120,48 @@ let g:session_autoload = "no"
 let g:session_autosave = "no"
 let g:session_command_aliases = 1
 
-""nnoremap ;f <Esc>:Lex<CR>:vertical resize 30<CR><CR>
-""nnoremap <leader>ex <Esc>:Ex<CR>
+"nnoremap ;f <Esc>:Lex<CR>:vertical resize 30<CR><CR>
+"nnoremap <leader>ex <Esc>:Ex<CR>
 ""let g:netrw_liststyle = 3
+"let g:netrw_banner=0        " disable annoying banner
+
 " NvimTreeToggle remap
  nnoremap ;f <Esc>:NvimTreeToggle<CR>:vertical resize 30<CR><CR>
 
+"checkout these fzf keybinds
+" PLUGIN: FZF
+"nnoremap <silent> <Leader>b :Buffers<CR>
+nnoremap <silent> <C-p> :Files<CR>
+"nnoremap <silent> <Leader>rg :Rg<CR> "ripgrep grep lines
+"nnoremap <silent> <Leader>ag :Ag<CR> "project finder
+"nnoremap <silent> <Leader>/ :BLines<CR> "line finder
+"nnoremap <silent> <Leader>/ :Lines<CR> "line finder
+"nnoremap <silent> <Leader>' :Marks<CR> "marked files find
+"nmap <Leader>t :BTags<CR>  "tag finder 
+"nmap <Leader>T :Tags<CR>   "search for tags across project
+"nnoremap <silent> <Leader>g :Commits<CR>
+"nnoremap <silent> <Leader>H :Helptags<CR> "help finder
+"nnoremap <silent> <Leader>hh :History<CR> "history finder
+"nnoremap <silent> <Leader>h: :History:<CR> "history finder
+"nnoremap <silent> <Leader>h/ :History/<CR> "history finder
+"nnoremap <silent> <Leader>h/ :Maps<CR> "mappings finder
+"nnoremap <silent> <Leader>h/ :Commands<CR> "commands finder
+"nnoremap <silent> <Leader>h/ :Filetypes<CR> "Filetypes finder
+
+"set grep to ripgrep
+set grepprg=rg\ --vimgrep\ --smart-case\ --follow
+
+" for fish use this to use fzf :set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
+" for bash/zsh use this export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
+" and remove the upper lines
+
+command! -bang -nargs=* Rg
+  \ call fzf#vim#grep(
+  \   'rg --column --line-number --hidden --ignore-case --no-heading --color=always '.shellescape(<q-args>), 1,
+  \   <bang>0 ? fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}, 'up:60%')
+  \           : fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}, 'right:50%:hidden', '?'),
+  \   <bang>0)
 ]])
-
--- from ThePrimeagen ------------------------------------------
-
--- best remap from the ThePrimeagen thanks brudda
--- greatest remap ever
-vim.keymap.set('x', "<leader>p", "\"_dP")
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set('n', "<leader>y", "\"+y")
-vim.keymap.set('v', "<leader>y", "\"+y")
-vim.keymap.set('n', "<leader>Y", "\"+Y")
-
-vim.keymap.set('n', "<leader>d", "\"_d")
-vim.keymap.set('v', "<leader>d", "\"_d")
-
-vim.keymap.set('v', "<leader>d", "\"_d")
-
--- half page up/down with centered view
-vim.keymap.set('n', "<C-u>", "<C-u>zz")
-vim.keymap.set('n', "<C-d>", "<C-d>zz")
-
--- searching forward or back with half page up/down
-vim.keymap.set('n', "n", "nzzzv")
-vim.keymap.set('n', "N", "nzzzv")
-
-------------------------------------------------------------------
---
--- shortcuts for commenting
-vim.keymap.set('n', "<C-/>", vim.cmd.Commentary)
-vim.keymap.set('v', "<C-/>", vim.cmd.Commentary)
-
--- undotree keymaps
-vim.keymap.set('n', "<leader>un", ":UndotreeToggle<CR>")
-
--- maximizer keymap
-vim.keymap.set('n', "<leader>mt", ":MaximizerToggle<CR>")
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>te', ":Telescope<CR>")
