64d080ab9b5c9a5e5d8d5c698cf613a5f28103e1
LegioN2004 <sunnybaruasins@gmail.com>
2023-03-11 15:39:24 +0530

    aiorntant

--- nvim/.config/nvim/lua/legion/lualine.lua
+++ nvim/.config/nvim/lua/legion/lualine.lua
@@ -5,7 +5,7 @@ lualine.setup {
 	options = {
 		icons_enabled = true,
 		-- theme = 'solarized_dark',
-		theme = 'auto',
+		theme = 'onedark',
 		section_separators = { left = '', right = '' },
 		component_separators = { left = '', right = '' },
 		disabled_filetypes = {}
