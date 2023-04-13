87aaf9b6d81290e3c81ab11cf50551f3590e9430
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-04-13 18:00:48 +0530

    fix the noice(folke/noice.nvim plugin for usage

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -188,17 +188,17 @@ return {
 					},
 					lualine_x = {
 				-- stylua: ignore
-				{
-				  function() return require("noice").api.status.command.get() end,
-				  cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-				  color = fg("Statement")
-				},
-				-- stylua: ignore
-				{
-				  function() return require("noice").api.status.mode.get() end,
-				  cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-				  color = fg("Constant") ,
-				},
+				-- {
+				--   function() return require("noice").api.status.command.get() end,
+				--   cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+				--   color = fg("Statement")
+				-- },
+				-- -- stylua: ignore
+				-- {
+				--   function() return require("noice").api.status.mode.get() end,
+				--   cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+				--   color = fg("Constant") ,
+				-- },
 						{
 							require("lazy.status").updates,
 							cond = require("lazy.status").has_updates,
