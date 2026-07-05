160863f24d9962cbe6f6903f3cc9eac0288edd56
legion2004 <milindabarua04@gmail.com>
2026-07-05 10:56:27 +0530

    refactor keymaps.lua for improved readability and organization

--- nvim/.config/nvim/lua/sethy/core/keymaps.lua
+++ nvim/.config/nvim/lua/sethy/core/keymaps.lua
@@ -22,7 +22,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("v", "p", '"_dp', opts)
 
 -- Yank to system clipboard with <leader>y (normal/visual)
-vim.keymap.set({"n", "v"}, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
 
 -- leader d delete to blackhole (does not affect any register)
@@ -44,23 +44,27 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "x", '"_x', opts)
 
 -- Replace the word cursor is on globally
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "Replace word cursor is on globally" })
+vim.keymap.set(
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Replace word cursor is on globally" }
+)
 
 -- Executes shell command from in here making file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "makes file executable" })
 
 -- Hightlight yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.hl.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 -- tab stuff
-vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>")   --open new tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>") --open new tab
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>") --close current tab
 -- Tab movement (your file uses <TAB> and <S-TAB> for tabn/tabp, but not these)
 vim.api.nvim_set_keymap("n", "<tab>", ":tabnext<Return>", { noremap = true, silent = true })
@@ -69,19 +73,19 @@ vim.keymap.set("n", "<leader>tnt", "<cmd>tabnew %<CR>") --open current tab in ne
 
 -- Copy filepath to the clipboard
 vim.keymap.set("n", "<leader>fp", function()
-  local filePath = vim.fn.expand("%:~") -- Gets the file path relative to the home directory
-  vim.fn.setreg("+", filePath) -- Copy the file path to the clipboard register
-  print("File path copied to clipboard: " .. filePath) -- Optional: print message to confirm
+	local filePath = vim.fn.expand("%:~") -- Gets the file path relative to the home directory
+	vim.fn.setreg("+", filePath) -- Copy the file path to the clipboard register
+	print("File path copied to clipboard: " .. filePath) -- Optional: print message to confirm
 end, { desc = "Copy file path to clipboard" })
 
 -- Toggle LSP diagnostics visibility
 local isLspDiagnosticsVisible = true
 vim.keymap.set("n", "<leader>lx", function()
-    isLspDiagnosticsVisible = not isLspDiagnosticsVisible
-    vim.diagnostic.config({
-        virtual_text = isLspDiagnosticsVisible,
-        underline = isLspDiagnosticsVisible
-    })
+	isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+	vim.diagnostic.config({
+		virtual_text = isLspDiagnosticsVisible,
+		underline = isLspDiagnosticsVisible,
+	})
 end, { desc = "Toggle LSP diagnostics" })
 
 -- Remap space as leader key (already set in your file, but not this exact mapping)
@@ -107,7 +111,6 @@ vim.api.nvim_set_keymap("n", "<leader>tqa", ":qa!<CR>", { noremap = true, silent
 vim.api.nvim_set_keymap("n", "<leader>twq", ":wq!<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ts", ":so<CR>", { noremap = true, silent = true })
 
-
 -- Buffer movement (not present)
 vim.api.nvim_set_keymap("n", "<leader><tab>", ":bnext<Return>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader><S-tab>", ":bprevious<Return>", { noremap = true, silent = true })
@@ -125,9 +128,9 @@ vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", { noremap =
 -- vim.keymap.set("n", "<leader>ps", function()
 --   require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
 -- end)
--- vim.keymap.set("n", ";r", function()
---   require("telescope.builtin").live_grep()
--- end)
+vim.keymap.set("n", "<leader>tlg", function()
+	require("telescope.builtin").live_grep()
+end)
 -- vim.keymap.set("n", "<leader>?", function()
 --   require("telescope.builtin").current_buffer_fuzzy_find()
 -- end)
@@ -140,31 +143,30 @@ vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", { noremap =
 -- vim.keymap.set("n", "<leader>re", function()
 --   require("telescope.builtin").resume()
 -- end)
--- vim.keymap.set("n", "<leader>bb", function()
---   require("telescope.builtin").buffers()
--- end)
+vim.keymap.set("n", "<leader>tb", function()
+	require("telescope.builtin").buffers()
+end)
 -- vim.keymap.set("n", "<leader>cs", function()
 --   require("telescope.builtin").lsp_document_symbols()
 -- end)
 -- vim.keymap.set("n", "<leader>fr", function()
 --   require("telescope.builtin").oldfiles()
 -- end)
--- vim.keymap.set("n", "<leader>km", function()
---   require("telescope.builtin").keymaps()
--- end)
+vim.keymap.set("n", "<leader>tkm", function()
+	require("telescope.builtin").keymaps()
+end)
 -- vim.keymap.set("n", "<leader>;h", function()
 --   require("telescope.builtin").help_tags()
 -- end)
--- vim.keymap.set("n", "<leader>fP", function()
---   require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
--- end)
+vim.keymap.set("n", "<leader>tff", function()
+	require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+end)
 -- vim.keymap.set("n", "<leader>;s", function()
 --   require("telescope.builtin").treesitter()
 -- end)
 -- vim.keymap.set("n", "<leader>;d", function()
 --   require("telescope.builtin").diagnostics()
 -- end)
-
 
 -- Auto save keymap
 vim.api.nvim_set_keymap("n", "<leader>ast", ":ASToggle<CR>", {})
